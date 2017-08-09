d50c06c9bbd458dbc6717696376c1af3c2493193
王玮 <316817495@qq.com>
2017-08-09 17:41:09 +0800

    启动页面动画-暂存

--- src/components/theNewPage/start-up.vue
+++ src/components/theNewPage/start-up.vue
@@ -2,7 +2,10 @@
 <template>
     <div class="startUp">
             <div class="bgimg"> </div>
-            <div class="logoimg"></div>
+            <!-- <div class="logoimg"></div> -->
+
+            <div class="logofull"></div>
+            <div class="logotext"></div>
            
             <div class="bottomFontStyle">
                 Copyright © 2017 xinyi-移动产品部.前端团队
