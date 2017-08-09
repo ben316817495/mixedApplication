b1ec62f968645aff76951fdb6a5b9276c55af976
王玮 <316817495@qq.com>
2017-08-09 14:15:58 +0800

    登陆页面

--- src/components/theNewPage/login.vue
+++ src/components/theNewPage/login.vue
@@ -1,6 +1,13 @@
 
 <template>
     <div class="loginPage">
-
+            <div class="bgimg"> </div>
+            <div class="logoimg"></div>
+            <div class="inputDevice">
+                
+            </div>
+            <div class="bottomFontStyle">
+                Copyright © 2017 xinyi-移动产品部.前端团队
+            </div>
     </div>
 </template>
