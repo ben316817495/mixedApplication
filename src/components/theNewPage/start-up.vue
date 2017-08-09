51b0eeb1dfbaf3cbfb7e32e47a35b4be5932cf26
王玮 <316817495@qq.com>
2017-08-09 17:48:00 +0800

    初稿01

--- src/components/theNewPage/start-up.vue
+++ src/components/theNewPage/start-up.vue
@@ -6,6 +6,7 @@
 
             <div class="logofull"></div>
             <div class="logotext"></div>
+            <div class="logotext_topsmall">Engineer of browser side</div>
            
             <div class="bottomFontStyle">
                 Copyright © 2017 xinyi-移动产品部.前端团队
