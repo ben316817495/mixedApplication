15e800b69d7cc7ecf3a351792fa6d6dc74d1d2aa
王玮 <316817495@qq.com>
2017-08-09 15:46:21 +0800

    登陆页面布局

--- src/components/theNewPage/login.vue
+++ src/components/theNewPage/login.vue
@@ -4,7 +4,26 @@
             <div class="bgimg"> </div>
             <div class="logoimg"></div>
             <div class="inputDevice">
+                <div class="inputBox_container">
+                    <div class="inputBox_line">
+                        <div class="inputBox_icon"><i class="iconfont icon-people"></i></div>
+                        <div class="inputBox_inputbox"><input class="inputBox_input" placeholder="请输入账号" /></div>
+                    </div>
+                     <div class="inputBox_line">
+                        <div class="inputBox_icon"><i class="iconfont icon-unlock2"></i></div>
+                        <div class="inputBox_inputbox"><input class="inputBox_input" placeholder="请输入密码" /></div>
+                    </div>
+                </div>
                 
+                <div class="inputBottom_button">
+                    <a class="weui-btn weui-btn_logincolor" href="javascript:">登陆</a>    
+                </div>
+
+                <div class="inputBottom_textLine">
+                    <div class="textLine_left">Forgot the password?</div>
+                    <div class="textLine_right">Not registered yet?</div>
+                </div>
+
             </div>
             <div class="bottomFontStyle">
                 Copyright © 2017 xinyi-移动产品部.前端团队
