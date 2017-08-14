f62264139888e52b63369ac1a1d40d21c189e910
王玮 <316817495@qq.com>
2017-08-14 14:41:47 +0800

    个人页面样式暂存

--- src/components/theNewPage/myPage.vue
+++ src/components/theNewPage/myPage.vue
@@ -1,18 +1,17 @@
 <template>
     <div class="mypage">
         
-        <div class="page__hd">
+        <div class="page__hd topHeaderBox">
             
             <div class="imgHeader">
                 <img src="../../assets/img/defu_img.jpg" />
             </div>
 
-            <h1 class="page__title">Panel</h1>
-            <p class="page__desc">面板</p>
         </div>
        
        
         <div class="page__hd headerBottomText">
+            <h4>vincent178</h4>
         </div>
 
 
